aa7188f1e1959e708bf8a3c8f4c07de249047fbf
Jakub Arnold <darthdeus@gmail.com>
2022-12-26 01:04:30 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub use itertools::Itertools;
+// pub use nanoserde::{SerJson, DeJson};
+pub use serde::{Deserialize, Serialize};
+
+pub use macroquad::prelude::*;
+
+pub use std::collections::HashMap;
+
+pub use egui;
+pub use egui_macroquad;
+pub use serde_json;
+pub use serde;
+pub use macroquad;
