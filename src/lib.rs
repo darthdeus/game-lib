29a31319d637e9ab100c88a8d85860610d071b61
Jakub Arnold <darthdeus@gmail.com>
2022-12-26 20:28:38 +0100

    Add load/save json from/to file

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,32 @@ pub use egui_macroquad;
 pub use serde_json;
 pub use serde;
 pub use macroquad;
+pub use anyhow::Result;
+
+
+pub fn load_json_from_file_or_default<T: for<'a> Deserialize<'a> + Default>(
+    path: &str,
+) -> T {
+    let doit = || -> Result<T> {
+        let data = std::fs::read_to_string(path)?;
+        Ok(serde_json::from_str::<T>(&data)?)
+    };
+
+    doit().unwrap_or_else(|err| {
+        error!("Error: {}", err);
+        T::default()
+    })
+}
+
+pub fn save_json_to_file<T: Serialize>(path: &str, val: &T) {
+    let doit = || -> Result<()> {
+        let json_data = serde_json::to_string_pretty(val)?;
+        std::fs::write(path, json_data)?;
+
+        Ok(())
+    };
+
+    doit().unwrap_or_else(|err| {
+        error!("Error: {}", err);
+    });
+}
